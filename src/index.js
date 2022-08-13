406a58d6dd55dd8b160338f495f8379053d77038
joelrummel <joelrummel@yahoo.com>
2022-08-12 20:44:09 -0400

    Committing everything on working tree

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
+import Home from './Home';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#a65a4b'
+        },
+        secondary: {
+            main: '#4b97a6'
+        },
+        background: {
+            default: '#4b97a6'
+        }
+    },
+    typography: {
+        fontFamily: ['Nunito', 'sans-serif'].join(','),
+        h1: {
+            fontFamily: ['Play', 'sans-serif'].join(',')
+        },
+        h2: {
+            fontFamily: ['Play', 'sans-serif'].join(',')
+        },
+        h3: {
+            fontFamily: ['Play', 'sans-serif'].join(',')
+        },
+        h4: {
+            fontFamily: ['Play', 'sans-serif'].join(',')
+        },
+        h5: {
+            fontFamily: ['Play', 'sans-serif'].join(',')
+        },
+        h6: {
+            fontFamily: ['Play', 'sans-serif'].join(',')
+        },
+        body2: {
+            color: 'grey'
+        },
+        link: {
+            color: '#4b97a6'
+        }
+    }
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <CssBaseline />
         <BrowserRouter>
-            <App />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Home />
+            </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
